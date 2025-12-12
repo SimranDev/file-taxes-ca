31f4bb9a19125c3c69fbc7e5e5b864846cc6a145
Simran Singh <81152234+SimranDev@users.noreply.github.com>
2025-12-12 23:03:49 +0530

    feat: replace placeholder with UnderDevelopment component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import UnderDevelopment from "../components/UnderDevelopment";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 pt-24">
+        <UnderDevelopment />
+      </main>
+    </div>
+  );
 }
